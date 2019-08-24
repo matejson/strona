aa7d63c0cc1586f75a8b8ef4f387872c7c5e42b5
Matejson <mateusz.zaborowskii@gmail.com>
2019-08-24 23:39:21 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+        
+  jQuery(document).ready(function() {
+var offset = 700;
+var duration = 500;
+jQuery(window).scroll(function() {
+if (jQuery(this).scrollTop() > offset) {
+jQuery('.back-to-top').fadeIn(duration);
+} else {
+jQuery('.back-to-top').fadeOut(duration);
+}
+});
+jQuery('.back-to-top').click(function(event) {
+event.preventDefault();
+jQuery('html, body').animate({scrollTop: 0}, duration);
+return false;
+})
+});
+
+// videos play and pause
+
+const videos = document.querySelectorAll(".videos");
+
+videos.forEach(video =>{
+               video.addEventListener("mouseover", function (){
+               this.play()
+               })
+    
+    video.addEventListener("mouseout", function(){
+        this.pause()
+    })
+               })
